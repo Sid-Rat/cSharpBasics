7ab585a6c5c908d93b8068b12367521d24d9f4ad
Doc <sidratgames@protonmail.com>
2022-02-07 10:51:28 -0500

    added For

--- 07_Loops/For/Program.cs
+++ 07_Loops/For/Program.cs
@@ -1,4 +1,9 @@
 ﻿int number = 100;
-for (int i = 0; i < number; i++)
+for (int i = 0; i <= number; i++)
 {Console.WriteLine(i);}
 
+int numberTwo = 200;
+for (int i = 1; i <= numberTwo; i++)
+{numberTwo = numberTwo - 1;
+Console.WriteLine(numberTwo);
+Console.WriteLine($"i is {i}");}
