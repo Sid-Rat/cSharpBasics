cc60feb199ba6b0fafd6e2fbdf0d329d499952d0
Doc <sidratgames@protonmail.com>
2022-02-07 10:36:01 -0500

    added 07_Loops\For

---
+++ 07_Loops/For/Program.cs
@@ -0,0 +1,4 @@
+﻿int number = 100;
+for (int i = 0; i < number; i++)
+{Console.WriteLine(i);}
+
---
+++ 07_Loops/While/Program.cs
@@ -0,0 +1,9 @@
+﻿// Declare a variable
+int number = 0;
+
+//Keyword Condition
+while (number <=100) //If condition is true; Loop
+{
+    Console.WriteLine(number);
+    number = number + 5;
+}
